d7e060eac8d9ed93af128e6f35f1e5db5c6bf920
HUNG_DOAN\hungk <hung.doan@cpm-vietnam.com.vn>
2021-12-31 20:22:45 +0700

    v.1.1

---
+++ rest-spread/index.js
@@ -0,0 +1,50 @@
+// var arg1 = [
+//     1,2,3
+// ]
+// var arg2 = [
+//     4,5,6
+// ]
+//Spread
+// var numbers = [
+//     ...arg1,
+//     ...arg2
+// ]
+// for(let num of numbers){
+//     console.log(num);
+// }
+// function getNumber([a,b,c,...rest]){
+//     console.log(rest);
+// }
+// getNumber(numbers);
+
+var obj1 = {
+    name: "Hung Doan"
+}
+var obj2 = {
+    age: 27
+}
+//Spread
+// var students = {
+//     ...obj1,
+//     ...obj2
+// }
+// // console.log(students);
+
+// function getStudent(a){
+//     console.log(a);
+// }
+// getStudent(students);
+
+
+//Destructuring
+// var array = [1,2,3,4];
+// var [a,b,c,d] = array;
+// console.log(c);
+
+// var object = {
+//     name: "Hung Doan",
+//     age: 27
+// }
+
+// var {name,age} = object;
+// console.log(name);
